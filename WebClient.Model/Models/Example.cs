778683e810cee257247c5f1b21d34f09e818bd59
LeChuong <LeChuong@DESKTOP-4VBD8GO>
2017-10-15 21:51:26 +0700

    hello nhi, file class thi du, bam commit all la upp len het may file trong changes

---
+++ WebClient.Model/Models/Example.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebClient.Model.Models
+{
+    class Example
+    {
+        string abc;//vi du vay hen, gio up len git ne
+    }
+}
